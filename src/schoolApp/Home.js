3d0d3cb5c1301920c7af2d1d406ad1dc739a1dc1
annie <anniey9@hotmail.com>
2019-12-07 13:33:56 -0500

    carousel-indicators problem

--- src/schoolApp/Home.js
+++ src/schoolApp/Home.js
@@ -9,7 +9,11 @@ export default class Home extends Component {
         return (
         	<div>
 	            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-				  
+				  <ol class="carousel-indicators">
+				    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				  </ol>
 				  <div className="carousel-inner Images">
 				    <div className="carousel-item active">
 				      <img src="pic2.jpg" className="d-block w-100" alt="..." />
