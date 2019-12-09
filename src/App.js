4d95f13cda41e6c8e9dd39e8af7529707158488a
CrabcakeODA <46054396+CrabcakeODA@users.noreply.github.com>
2019-12-08 22:03:36 -0500

    new language system

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
-// import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
-// import 'typeface-roboto';
 
 import TopBar from './schoolApp/TopBar.js';
 import Home from './schoolApp/Home.js';
@@ -14,33 +12,54 @@ import OnlineTranslation from './schoolApp/OnlineTranslation.js';
 import OnlineResources from './schoolApp/OnlineResources.js';
 import Footer from './schoolApp/Footer.js';
 
-export default class App extends Component {
-  constructor(props){
-        super(props);
-        this.state = {
+import { withTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
-        };
-    }
-    render(){
-        return (
-            <BrowserRouter>
-              <div>
-                <TopBar />
-              </div>
-              <Switch>
-                <Route path="/" exact component={ Home }/>
-                <Route path="/home" component={ Home }/>
-                <Route path="/about-us" component={ AboutUs }/>
-                <Route path="/contact-us" component={ ContactUs }/>
-                <Route path="/courses" component={ Courses }/>
-                <Route path="/online-translation" component={ OnlineTranslation }/>
-                <Route path="/online-resources" component={ OnlineResources }/>
-                <Redirect from="/*" to="/"/>
-              </Switch>
-              <div>
-                <Footer />
-              </div>
-            </BrowserRouter>
-        )
-    }
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+
+  render() {
+    const { t } = this.props;
+
+    return (
+      <BrowserRouter>
+        <div>
+          <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+            <button onClick={() => this.handleClick('en')} >
+              English
+            </button>
+            <button onClick={() => this.handleClick('zh')} >
+              Chinese
+            </button>
+          </nav>
+          <h3>{t('languageExample')}</h3>
+          <TopBar />
+        </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/contact-us" component={ContactUs} />
+          <Route path="/courses" component={Courses} />
+          <Route path="/online-translation" component={OnlineTranslation} />
+          <Route path="/online-resources" component={OnlineResources} />
+          <Redirect from="/*" to="/" />
+        </Switch>
+        <div>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
+
+export default withTranslation()(App);
