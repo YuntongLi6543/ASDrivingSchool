f5f41d3eb6b8acda7c0b7faca656dc540af848ab
CrabcakeODA <46054396+CrabcakeODA@users.noreply.github.com>
2020-02-16 23:47:05 -0500

    fix page about us

--- src/schoolApp/AboutUs.js
+++ src/schoolApp/AboutUs.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -14,89 +14,89 @@ class AboutUs extends Component {
 		i18next.changeLanguage(lang);
 	}
 
-    render() {
+	render() {
 		const { t } = this.props;
-        return (
-        	<div className="AboutUs">
-	            <Container>
-		            <Row>
-		            	<Col xs={12} md={12}>
-					      <h2 className="page-title">{t('aboutUs.aboutASDrivingSchool')}</h2>
-					    </Col>
+		return (
+			<div>
+				<Container>
+					<Row>
+						<Col xs={12} md={12}>
+							<h2 className="page-title">{t('aboutUs.aboutASDrivingSchool')}</h2>
+						</Col>
 						{/* introduction */}
-					    <Col xs={12} md={5}>
-					       <Card.Img className="schoolImage" src={require(`../assets/img/asdrivingschool.png`)} />
-					    </Col>
-
-					    <Col xs={12} md={7}>
-					      	<p className="Content">
-							  {t('aboutUs.aboutASDrivingSchoolContent.para1')}
-							  <br />
-							  {t('aboutUs.aboutASDrivingSchoolContent.para2')}
-							  <br />
-							  {t('aboutUs.aboutASDrivingSchoolContent.para3')}
-					    	</p>
-					    </Col>
-		  			</Row>
-	            </Container>
-
-	            <div className="ThreeCoachInfo">
-	            <Container>
-	           		<Row>
-	            		<Col xs={12} md={12} className="title">
-					    	<h2 className="AU-Header-Coach">{t('aboutUs.featuredInstructors')}</h2>
+						<Col xs={12} md={5}>
+							<Card.Img className="schoolImage" src={require(`../assets/img/asdrivingschool.png`)} />
 						</Col>
-		  				{/* coach-Zhang */}
-		  			<Row className="coach-zhang">
+
 						<Col xs={12} md={7}>
-					      	<p className="Content">
-							{t('aboutUs.coachZhang.para1')}
-					      	<br /><br />{t('aboutUs.coachZhang.para2')}
-					      	<br /><br />{t('aboutUs.coachZhang.para3')}
-					      	<br /><br />{t('aboutUs.coachZhang.tel')}
-					    	</p>
-					    </Col>
-						<Col xs={12} md={5} >
-						    <Card.Img className="Image" src={require(`../assets/img/coach-zhang.png`)} />
+							<p className="content">
+								{t('aboutUs.aboutASDrivingSchoolContent.para1')}
+							</p>
+							<p className="content">
+								{t('aboutUs.aboutASDrivingSchoolContent.para2')}
+							</p>
+							<p className="content">
+								{t('aboutUs.aboutASDrivingSchoolContent.para3')}
+							</p>
 						</Col>
 					</Row>
-					</Row>
 				</Container>
-				    {/* coach-Zhou */}
-	
-				<Container className="coach-zhou">
-					<Row>
-					    <Col xs={12} md={5}>
-					       <Card.Img className="Image" src={require(`../assets/img/coach-zhou.png`)} />
-					    </Col>
 
-					    <Col xs={12} md={7}>
-					      	<p className="Content">{t('aboutUs.coachZhou.para1')}
-					      	<br /><br />{t('aboutUs.coachZhou.para2')}
-					      	<br /><br />{t('aboutUs.coachZhang.tel')}
-					    	</p>
-					    </Col>
+				<Container className="mb-5">
+					<Row>
+						<Col xs={12} md={12}>
+							<h2 className="page-title">{t('aboutUs.featuredInstructors')}</h2>
+						</Col>
+						{/* coach-Zhang */}
+						<Row>
+							<Col xs={12} md={7}>
+								<p className="content">
+									{t('aboutUs.coachZhang.para1')}
+									<br /><br />{t('aboutUs.coachZhang.para2')}
+									<br /><br />{t('aboutUs.coachZhang.para3')}
+									<br /><br />{t('aboutUs.coachZhang.tel')}
+								</p>
+							</Col>
+							<Col xs={12} md={5} >
+								<Card.Img className="coachImage" src={require(`../assets/img/coach-zhang.png`)} />
+							</Col>
+						</Row>
 					</Row>
 				</Container>
-				
-				 {/* coach-Hao */}
-				<Container className="coach-hao">
+				{/* coach-Zhou */}
+
+				<Container className="mb-5">
 					<Row>
-					    <Col xs={12} md={7}>
-					      	<p className="Content">{t('aboutUs.coachHao.para1')}
-					      	<br /><br />{t('aboutUs.coachHao.para2')}
-					      	<br /><br />{t('aboutUs.coachHao.tel')}
-					    	</p>
-					    </Col>
-					    <Col xs={12} md={5}>
-					       <Card.Img className="Image" src={require(`../assets/img/coach-hao.png`)} />
-					    </Col>
+						<Col xs={12} md={5}>
+							<Card.Img className="coachImage" src={require(`../assets/img/coach-zhou.png`)} />
+						</Col>
+
+						<Col xs={12} md={7}>
+							<p className="content">{t('aboutUs.coachZhou.para1')}
+								<br /><br />{t('aboutUs.coachZhou.para2')}
+								<br /><br />{t('aboutUs.coachZhang.tel')}
+							</p>
+						</Col>
 					</Row>
-	            </Container>
-	            </div>
-  			</div>
-        );
-    }
+				</Container>
+
+				{/* coach-Hao */}
+				<Container className="mb-5">
+					<Row>
+						<Col xs={12} md={7}>
+							<p className="content">{t('aboutUs.coachHao.para1')}
+								<br /><br />{t('aboutUs.coachHao.para2')}
+								<br /><br />{t('aboutUs.coachHao.tel')}
+							</p>
+						</Col>
+						<Col xs={12} md={5}>
+							<Card.Img className="coachImage" src={require(`../assets/img/coach-hao.png`)} />
+						</Col>
+					</Row>
+				</Container>
+			</div >
+		);
+	}
 }
 
 export default withTranslation()(AboutUs);
