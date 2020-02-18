11009cc52a2709dc28da1311141f2a185aef86d2
CrabcakeODA <46054396+CrabcakeODA@users.noreply.github.com>
2020-02-18 18:25:19 -0500

    Create TestData.js

---
+++ src/assets/data/TestData.js
@@ -0,0 +1,25 @@
+export const englishTest = [
+    {
+        key: '0',
+        src: "assets/img/test/",
+        question: '',
+        optionA: '',
+        optionB: '',
+        optionC: '',
+        optionD: '',
+        answer: '',
+    }
+];
+
+export const chineseTest = [
+    {
+        key: '0',
+        src: "assets/img/test/",
+        question: '',
+        optionA: '',
+        optionB: '',
+        optionC: '',
+        optionD: '',
+        answer: '',
+    }
+];
