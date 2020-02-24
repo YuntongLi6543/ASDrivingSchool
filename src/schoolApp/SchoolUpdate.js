db74087d31755e105d7c4f30ea7dc32bec98cf3c
CrabcakeODA <46054396+CrabcakeODA@users.noreply.github.com>
2020-02-24 00:06:54 -0500

    Update SchoolUpdate.js

--- src/schoolApp/SchoolUpdate.js
+++ src/schoolApp/SchoolUpdate.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
 import {
     EmailShareButton,
     TwitterShareButton,
